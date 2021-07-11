58bf1a6940d196f3078cc4dd724297adcfed33a6
muhammednasimda <muhammednasimda@gmail.com>
2021-07-11 19:37:32 +0530

    some

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const tiktok = require("tiktok-app-api");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { getVideoMeta } = require("tiktok-scraper");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/api/", async (req, res) => {
-  const tiktokApp = await tiktok();
-
-  const user = await tiktokApp.getUserByName("tiktok");
-  const userInfo = await tiktokApp.getUserInfo(user);
-
-  res.send(userInfo.followerCount);
+app.get("/api", async (req, res) => {
+  try {
+    const resp = await getVideoMeta(
+      "https://www.tiktok.com/@scout2015/video/6718335390845095173",
+      {}
+    );
+    return res.send(resp);
+  } catch (error) {
+    return res.send(error);
+  }
 });
 
 app.listen(PORT);
